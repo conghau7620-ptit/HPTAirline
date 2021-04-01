4a496a0805df24ecf38159a347400b8b852fc18e
conghau7620-ptit <conghau7620.ptit@gmail.com>
2021-04-01 12:00:03 +0700

    cap nhat package connection

--- HPTAirLine/src/controller/Controller.java
+++ HPTAirLine/src/controller/Controller.java
@@ -5,10 +5,34 @@
  */
 package controller;
 
+import connection.LoadData;
+import java.util.ArrayList;
+import model.ChuyenBay;
+import model.Ghe;
+import model.HoaDon;
+import model.KhachHang;
+import model.MayBay;
+import model.NhanVien;
+import model.SanBay;
+import model.TaiKhoan;
+import model.Ve;
+
 /**
  *
  * @author conghau
  */
 public class Controller {
+    public static ArrayList<ChuyenBay> arrayListChuyenBay = new ArrayList<ChuyenBay>();
+    public static ArrayList<Ghe> arrayListGhe = new ArrayList<Ghe>();
+    public static ArrayList<HoaDon> arrayListHoaDon = new ArrayList<HoaDon>();
+    public static ArrayList<KhachHang> arrayListKhachHang = new ArrayList<KhachHang>();
+    public static ArrayList<MayBay> arrayListMayBay = new ArrayList<MayBay>();
+    public static ArrayList<NhanVien> arrayListNhanVien = new ArrayList<NhanVien>();
+    public static ArrayList<SanBay> arrayListSanBay = new ArrayList<SanBay>();
+    public static ArrayList<TaiKhoan> arrayListTaiKhoan = new ArrayList<TaiKhoan>();
+    public static ArrayList<Ve> arrayListVe = new ArrayList<Ve>();
     
+    public static void main(String[] args) {
+        new LoadData();
+    }
 }
